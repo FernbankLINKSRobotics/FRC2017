86bb2c9bd1b0fae3d7147e43ec1364fbd0098a8a
Astrobot314 <emilyburnskaurin@fernbanklinks.com>
2017-01-16 14:22:01 -0500

    Shooter subsystem

--- src/shooter/ShooterSubsystem.java
+++ src/shooter/ShooterSubsystem.java
@@ -1,20 +1,38 @@
 package shooter;
 
+import org.usfirst.frc.team4468.robot.CMap;
+
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.SpeedController;
+
 public class ShooterSubsystem {
 
-	//The Intake
-	public class intake{
-		
+	//motor controller for shooter
+	public static SpeedController shooter;
+
+	private static boolean beenPressed = false;
+	
+	public Encoder encoder;
+	
+	public ShooterSubsystem(SpeedController shooterMotor, Encoder encoder1){
+		shooter = shooterMotor;
+		encoder = encoder1;
 	}
 	
-	//The actual launcher
-	public class launcher{
-		
-	}
 	
-	//The total subsystem
-	public ShooterSubsystem(){
+	public static void set(Joystick stick){
 		
+		if(stick.getTrigger()){
+			if(!beenPressed ){
+				if(shooter.get() == 0){
+					shooter.set(1);	
+				}
+				else{
+					shooter.set(0);
+				}
+			}
+		}
 	}
 	
 }
